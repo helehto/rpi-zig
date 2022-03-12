7267ab4b837c101f3aa288bc265dc16a5a37ba3e
Henrik Lehtonen <henrik@vm86.se>
2022-03-12 19:22:35 +0100

    Force assembly wrapper functions to be inlined

--- arm.zig
+++ arm.zig
@@ -1,13 +1,13 @@
-pub fn mrs(comptime reg: []const u8) u64 {
+pub inline fn mrs(comptime reg: []const u8) u64 {
     var result: u64 = undefined;
     asm volatile ("mrs %[r], " ++ reg : [r]"=r"(result) :: "memory");
     return result;
 }
 
-pub fn msr(comptime reg: []const u8, value: u64) void {
+pub inline fn msr(comptime reg: []const u8, value: u64) void {
     asm volatile("msr " ++ reg ++ ", %[r]" :: [r]"r"(value) : "memory");
 }
 
-pub fn currentEL() u2 {
+pub inline fn currentEL() u2 {
     return @truncate(u2, mrs("currentEL") >> 2);
 }
